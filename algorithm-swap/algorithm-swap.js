5be886dee4f996f389c0a7ab1bcb99024ea12632
Boyul Kim <boyulkim1@gmail.com>
2021-03-29 14:46:53 -0700

    solved algo swap problem

--- algorithm-swap/algorithm-swap.js
+++ algorithm-swap/algorithm-swap.js
@@ -1,2 +1,15 @@
-const sampleArray = [5,4,1,2]
-console.log(sampleArray);
+const sampleArray = [5,4,1,2,7,9,3]
+let counter = 0;
+
+for(let index = 0; index<=sampleArray.length-1; index++) {
+  let indexValue = sampleArray[index]
+  for(let test = index+1; test<=sampleArray.length-1; test++) {
+    console.log('indexValue:', sampleArray[index], 'testValue:', sampleArray[test])
+    if(indexValue > sampleArray[test]) {
+      counter++
+      console.log('counter addded')
+    }
+  }
+}
+
+console.log(counter)
